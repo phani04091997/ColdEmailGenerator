5bc38d7d199395f7220778809eb0c8d310173651
phani_a <pindra829@gmail.com>
2024-12-11 16:28:47 -0600

    groq api key added

---
+++ app/chains.py
@@ -0,0 +1,8 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
---
+++ app/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+# Print "Hello" in Streamlit
+st.write("Hello")
+st.write("Hello")
